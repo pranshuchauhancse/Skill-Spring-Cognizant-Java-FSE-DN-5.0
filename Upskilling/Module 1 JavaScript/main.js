b615f28115143b21707daad29894fa926c6b88e3
Pranshu Chauhan <pranshu121005@gmail.com>
2026-06-30 22:28:08 +0530

    JavaScript Basics & Setup

---
+++ Upskilling/Module 1 JavaScript/main.js
@@ -0,0 +1,7 @@
+// Display a message in the browser console
+console.log("Welcome to the Community Portal");
+
+// Show an alert when the page is fully loaded
+window.onload = function () {
+    alert("Page loaded successfully!");
+};
