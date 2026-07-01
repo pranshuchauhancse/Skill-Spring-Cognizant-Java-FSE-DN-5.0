bda9bc610c070475b8ba53b6a42c17cfbc2d2f2a
Pranshu Chauhan <pranshu121005@gmail.com>
2026-07-01 14:18:11 +0530

    Calculator

---
+++ Upskilling/Module3Java/Calculator.java
@@ -0,0 +1,58 @@
+package Upskilling.Module3Java;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Input two numbers
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        // Choose operation
+        System.out.println("\nChoose Operation");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+
+        System.out.print("Enter your choice: ");
+        int choice = sc.nextInt();
+
+        switch (choice) {
+
+            case 1:
+                System.out.println("Result = " + (num1 + num2));
+                break;
+
+            case 2:
+                System.out.println("Result = " + (num1 - num2));
+                break;
+
+            case 3:
+                System.out.println("Result = " + (num1 * num2));
+                break;
+
+            case 4:
+                if (num2 != 0) {
+                    System.out.println("Result = " + (num1 / num2));
+                } else {
+                    System.out.println("Division by zero is not allowed.");
+                }
+                break;
+
+            default:
+                System.out.println("Invalid choice.");
+
+        }
+
+        sc.close();
+    }
+
+}
