42b4ff126f248caabe6c16c096b746334b227ce4
Pranshu Chauhan <pranshu121005@gmail.com>
2026-07-01 13:03:20 +0530

    Events Without Sessions

---
+++ Upskilling/Module 2 ANI SQL/Exercise25.sql
@@ -0,0 +1,11 @@
+SELECT
+    e.event_id,
+    e.title AS event_name,
+    e.city,
+    e.start_date,
+    e.status
+FROM Events e
+LEFT JOIN Sessions s
+    ON e.event_id = s.event_id
+WHERE
+    s.session_id IS NULL;
