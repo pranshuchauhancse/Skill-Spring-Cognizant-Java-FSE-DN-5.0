3106b67e0d47432a53e5ecb674d70bed351d25bb
Pranshu Chauhan <pranshu121005@gmail.com>
2026-07-01 14:28:32 +0530

    Data Type Demonstration

---
+++ Upskilling/Module3Java/DataTypeDemonstration.java
@@ -0,0 +1,23 @@
+package Upskilling.Module3Java;
+
+public class DataTypeDemonstration {
+    
+    public static void main(String[] args) {
+
+        // Primitive data types
+        int number = 100;
+        float decimal1 = 12.5f;
+        double decimal2 = 99.99;
+        char grade = 'A';
+        boolean isPassed = true;
+
+        // Display values
+        System.out.println("Integer Value : " + number);
+        System.out.println("Float Value   : " + decimal1);
+        System.out.println("Double Value  : " + decimal2);
+        System.out.println("Character     : " + grade);
+        System.out.println("Boolean Value : " + isPassed);
+
+    }
+
+}
