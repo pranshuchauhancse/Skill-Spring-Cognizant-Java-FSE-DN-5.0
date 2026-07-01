bc5e52c61cb8828d2d08d81ca5107f85adf0e19b
Pranshu Chauhan <pranshu121005@gmail.com>
2026-07-01 12:45:38 +0530

    Feedback Gap

---
+++ Upskilling/Module 2 ANI SQL/Exercise10.sql
@@ -0,0 +1,12 @@
+SELECT
+    e.event_id,
+    e.title AS event_name
+FROM Events e
+JOIN Registrations r
+    ON e.event_id = r.event_id
+LEFT JOIN Feedback f
+    ON e.event_id = f.event_id
+WHERE f.feedback_id IS NULL
+GROUP BY
+    e.event_id,
+    e.title;
