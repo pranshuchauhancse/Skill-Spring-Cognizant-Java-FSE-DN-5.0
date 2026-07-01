26a5cf1b64cc28db93e3e8125ede8784da943a8e
Pranshu Chauhan <pranshu121005@gmail.com>
2026-07-01 12:44:00 +0530

    Sessions per Upcoming Event

---
+++ Upskilling/Module 2 ANI SQL/Exercise8.sql
@@ -0,0 +1,13 @@
+SELECT
+    e.event_id,
+    e.title AS event_name,
+    COUNT(s.session_id) AS total_sessions
+FROM Events e
+LEFT JOIN Sessions s
+    ON e.event_id = s.event_id
+WHERE e.status = 'upcoming'
+GROUP BY
+    e.event_id,
+    e.title
+ORDER BY
+    e.event_id;
