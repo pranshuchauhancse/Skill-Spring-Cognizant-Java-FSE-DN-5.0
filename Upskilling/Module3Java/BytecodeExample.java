7c476a5c5ea82b9037d531e01cad101088364b2c
Pranshu Chauhan <pranshu121005@gmail.com>
2026-07-02 15:19:21 +0530

    Using javap to Inspect Bytecode

---
+++ Upskilling/Module3Java/BytecodeExample.java
@@ -0,0 +1,16 @@
+package Upskilling.Module3Java;
+
+public class BytecodeExample {
+
+    public void displayMessage() {
+        System.out.println("Hello from Bytecode Example!");
+    }
+
+    public static void main(String[] args) {
+
+        BytecodeExample obj = new BytecodeExample();
+        obj.displayMessage();
+
+    }
+
+}
