4bb471beab13e1da1ab8eb6c5b257ee91f82b9ad
Pranshu Chauhan <pranshu121005@gmail.com>
2026-07-02 15:20:43 +0530

    Decompile a Class File

---
+++ Upskilling/Module3Java/DecompileExample.java
@@ -0,0 +1,16 @@
+package Upskilling.Module3Java;
+
+public class DecompileExample {
+
+    public void displayMessage() {
+        System.out.println("Hello, Java!");
+    }
+
+    public static void main(String[] args) {
+
+        DecompileExample obj = new DecompileExample();
+        obj.displayMessage();
+
+    }
+
+}
