a5dc7dac873745e168430561687545375d171301
Pranshu Chauhan <pranshu121005@gmail.com>
2026-07-03 13:10:41 +0530

    Implementing the Factory Method Pattern

---
+++ Deepskilling/EngineeringConcepts/DesginPatternsandPrinciples/Exercise2/WordDocument.java
@@ -0,0 +1,9 @@
+package Deepskilling.EngineeringConcepts.DesginPatternsandPrinciples.Exercise2;
+
+public class WordDocument implements Document {
+
+    @Override
+    public void open() {
+        System.out.println("Word Document Opened.");
+    }
+}
