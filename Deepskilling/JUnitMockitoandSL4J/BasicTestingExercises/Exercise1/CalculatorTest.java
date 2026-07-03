62be4d009772e6da63bd384ec01d26798ce75751
Pranshu Chauhan <pranshu121005@gmail.com>
2026-07-03 14:13:22 +0530

    Create a Test Class

---
+++ Deepskilling/JUnitMockitoandSL4J/BasicTestingExercises/Exercise1/CalculatorTest.java
@@ -0,0 +1,11 @@
+package Deepskilling.JUnitMockitoandSL4J.BasicTestingExercises.Exercise1;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testSample() {
+        System.out.println("JUnit is working successfully.");
+    }
+}
