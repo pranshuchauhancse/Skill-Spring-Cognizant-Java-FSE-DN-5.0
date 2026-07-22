5ffd58e5059b6e07ebe4bfb8015269bee6724b7d
Pranshu Chauhan <pranshu121005@gmail.com>
2026-07-22 09:22:55 +0530

    D

--- Deepskilling/SpringDataJPAwithHibernate/1. spring-data-jpa-handson/Exercise 5/main/java/com/example/code/repository/CountryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.code.repository;
-
-import com.example.code.model.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CountryRepository extends JpaRepository<Country, String> {
-}
